79c2adf918029ea7ec52170790964c386fec796b
Zarzuket <davide.bajo@gmail.com>
2021-10-20 12:14:48 +0200

    snack 3

---
+++ snack3/index.php
@@ -0,0 +1,55 @@
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+<ul>
+	<?php foreach($posts as $data => $info) { ?>
+		<li>
+			<h3><?php echo $data; ?></h3>
+			<ul>
+				<?php foreach($info as $detail) { ?>
+				<li><?php echo $detail['title']; ?></li>
+                <li><?php echo $detail['author']; ?></li>
+                <li><?php echo $detail['text']; ?></li>
+				<?php } ?>
+			</ul>
+		</li>
+	<?php } ?>
+</ul>
